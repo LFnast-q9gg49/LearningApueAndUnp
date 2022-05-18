e42821162d5f46bd9e59409efe605f564daa84c0
LFnast <luyuritian@gmail.com>
2022-05-18 22:04:00 +0800

    test

---
+++ Day8/setitimer.cpp
@@ -0,0 +1,87 @@
+// slow cat
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/time.h> 
+
+const int CPS = 10;
+const int BFS = CPS;
+static volatile int loop = 0; 
+
+static void alrm_handler(int s){
+		alarm(1);
+		loop = 1;
+} 
+
+int main(int argc, char **argv){
+		int sfd = 0;
+		int dfd = 1;
+		char buf[BFS];
+		int len, ret, pos;
+		struct itimerval itv;
+
+		if (argc < 2){
+				fprintf(stdout, "Usage error: ./\n");
+				exit(1);
+		}
+
+		signal(SIGALRM, alrm_handler);
+		itv.it_interval.tv_sec = 1;
+		itv.it_interval.tv_usec = 0;
+		itv.it_value.tv_sec = 1;
+		itv.it_value.tv_usec = 0;
+		setitimer(ITIMER_REAL, &itv, nullptr);
+		do{
+				sfd = open(argv[1], O_RDONLY);
+				if (sfd < 0){
+						if(errno != EINTR){
+								perror("open error");
+								exit(1);
+						}
+
+				}
+		}while(sfd < 0);
+
+		while(1){
+				while(!loop){
+						pause();
+				}
+
+				loop = 0;
+
+				while((len = read(sfd, buf, BFS)) < 0){
+					if (len < 0){
+						if (errno != EINTR){
+								continue;
+						}
+						perror("read\n");
+						break;
+					}
+				}
+				if (len == 0){
+						break;
+				}
+				pos = 0;
+				while(len > 0){
+						ret = write(dfd, buf+pos, len);
+						if (ret < 0){
+								if (errno == EINTR){
+									continue;
+								}
+								perror("write");
+								exit(1);
+						}
+						pos += ret;
+						len -= ret;
+				}
+		}
+		close(sfd);
+		exit(0);
+}
