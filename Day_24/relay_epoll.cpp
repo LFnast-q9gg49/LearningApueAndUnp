6b3650dbf2d092479b6c40b7103b6073870623a0
LFnast <luyuhao19@mails.ucas.ac.cn>
2023-03-28 22:15:07 +0800

    20230328

---
+++ Day_24/relay_epoll.cpp
@@ -0,0 +1,200 @@
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cerrno>
+#include <sys/epoll.h>
+
+const char *TTY1 = "/dev/tty1";
+const char *TTY2 = "/dev/tty2";
+
+const int BUFSIZE = 100;
+
+enum {
+    STATE_R = 1,
+    STATE_W,
+    STATE_AUTO,
+    STATE_Ex,
+    STATE_T,
+};
+
+struct fsm_st{
+    int state;
+    int sfd;
+    int dfd;
+    int len;
+    int pos;
+    char buf[BUFSIZE];
+    char *err_str;
+};
+
+int max(int a, int b){
+    return a < b ? b: a;
+}
+
+int fsm_driver(struct fsm_st *fsm){
+    int ret;
+    switch (fsm->state) {
+        case STATE_R:
+            fsm->len = read(fsm->sfd, fsm->buf, BUFSIZE);
+            if (fsm->len == 0){
+                fsm->state = STATE_T;
+            }else if (fsm->len < 0){
+                if (errno == EAGAIN){
+                    fsm->state = STATE_R;
+                }else{
+                    fsm->err_str = "read";
+                    fsm->state = STATE_Ex;
+                }
+            } else{
+                fsm->pos = 0;
+                fsm->state = STATE_W;
+            }
+            break;
+        case STATE_W:
+            ret = write(fsm->dfd, fsm->buf + fsm->pos, fsm->len);
+            if (ret < 0){
+                if (errno == EAGAIN){
+                    fsm->state = STATE_W;
+                }else{
+                    fsm->err_str = "write";
+                    fsm->state = STATE_Ex;
+                }
+            } else{
+                fsm->pos += ret;
+                fsm->len -= ret;
+                if (fsm->len == 0){
+                    fsm->state = STATE_R;
+                }else{
+                    fsm->state = STATE_W;
+                }
+            }
+            break;
+        case STATE_Ex:
+            // perror
+            perror(fsm->err_str);
+
+            fsm->state = STATE_T;
+            break;
+        case STATE_T:
+            abort();
+
+        default:
+            break;
+    }
+}
+
+int relay(int fd_1, int fd_2){
+    int fd_1_save;
+    int fd_2_save;
+
+    struct fsm_st fsm12{}, fsm21{};
+
+    fd_1_save = fcntl(fd_1, F_GETFL);
+    fcntl(fd_1, F_SETFL, fd_1_save|O_NONBLOCK);
+
+    fd_2_save = fcntl(fd_2, F_GETFL);
+    fcntl(fd_1, F_SETFL, fd_2_save|O_NONBLOCK);
+
+    fsm12.state = STATE_R;
+    fsm12.sfd = fd_1;
+    fsm12.dfd = fd_2;
+
+    fsm21.state = STATE_R;
+    fsm21.sfd = fd_2;
+    fsm21.dfd = fd_1;
+
+    auto epfd = epoll_create(10);
+    if (epfd < 0){
+        perror("epoll create");
+        exit(1);
+    }
+
+    struct epoll_event ev;
+
+    ev.data.fd = fd_1;
+    ev.events = 0;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, fd_1, &ev);
+
+    ev.data.fd = fd_2;
+    ev.events = 0;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, fd_2, &ev);
+
+
+    while(fsm12.state != STATE_T || fsm21.state != STATE_T){
+        // task
+        ev.data.fd = fd_1;
+        ev.events = 0;
+
+        if (fsm12.state == STATE_R){
+            ev.events |= EPOLLIN;
+        }
+        if (fsm21.state == STATE_W){
+            ev.events |= EPOLLOUT;
+        }
+
+        epoll_ctl(epfd, EPOLL_CTL_MOD, fd_1, &ev);
+
+        ev.data.fd = fd_2;
+        ev.events = 0;
+        if (fsm12.state == STATE_W){
+            ev.events |= EPOLLOUT;
+        }
+        if (fsm21.state == STATE_R){
+            ev.events |= EPOLLIN;
+        }
+        epoll_ctl(epfd, EPOLL_CTL_MOD, fd_1, &ev);
+
+
+        // monitor
+        if (fsm12.state < STATE_AUTO || fsm21.state < STATE_AUTO) {
+            while (epoll_wait(epfd, &ev, 1, -1) < 0) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                perror("epoll wait");
+                exit(1);
+            }
+        }
+
+        // get result
+        if (ev.data.fd == fd_1 && ev.events & EPOLLIN\
+            || ev.data.fd == fd_2 && ev.events & EPOLLOUT\
+            || fsm12.state > STATE_AUTO){
+            fsm_driver(&fsm12);
+        }
+        if (ev.data.fd == fd_2 && ev.events & EPOLLIN\
+            || ev.data.fd == fd_1 && ev.events & EPOLLOUT\
+            || fsm21.state > STATE_AUTO) {
+            fsm_driver(&fsm21);
+        }
+    }
+
+    fcntl(fd_1, F_SETFL, fd_1_save);
+    fcntl(fd_2, F_SETFL, fd_2_save);
+
+    close(epfd);
+}
+
+int main(){
+    int fd_1, fd_2;
+    fd_1 = open(TTY1, O_RDWR);
+    if (fd_1 < 0){
+        perror("open");
+        exit(1);
+    }
+    write(1, "TTY1\n", 5);
+    fd_2 = open(TTY2, O_RDWR);
+    if (fd_2 < 0){
+        perror("open");
+        exit(1);
+    }
+    write(1, "TTY2\n", 5);
+
+    relay(fd_1, fd_2);
+
+    close(fd_1);
+    close(fd_2);
+    exit(0);
+}
+
