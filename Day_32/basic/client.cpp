c640c137de11fb7b4ce3a4b1a84e9747d0a47e78
LFnast <luyuhao19@mails.ucas.ac.cn>
2023-04-08 23:02:31 +0800

    20230408

--- Day_32/basic/client.cpp
+++ Day_32/basic/client.cpp
@@ -4,4 +4,55 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <ctime>
+#include <cerrno>
+#include <cstring>
 #include "proto.h"
+
+int main(int argc, char **argv){
+    FILE *fp;
+    int sd;
+    long long stamp;
+    struct sockaddr_in raddr{};
+
+    if (argc < 2){
+        fprintf(stderr, "Usage : ./client ip\n");
+        exit(1);
+    }
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd < 0){
+        perror("socket\n");
+        exit(1);
+    }
+
+    // bind
+    raddr.sin_family = AF_INET;
+    raddr.sin_port = htons(server_port);
+    inet_pton(AF_INET, argv[1], &raddr.sin_addr);
+
+    if (connect(sd, (struct sockaddr *)&raddr, sizeof(raddr)) < 0){
+        perror("connect");
+        exit(1);
+    }
+
+    fp = fdopen(sd, "r+");
+    if (fp == nullptr){
+        fprintf(stdout, "socket file open error : %s\n", strerror(errno));
+        // atexit
+        exit(1);
+    }
+
+    if (fscanf(fp, fmt_stamp, &stamp) < 1){
+        fprintf(stderr, "Bad Format\n");
+        exit(1);
+    } else{
+        fprintf(stdout, "stamp = %lld\n", stamp);
+    }
+
+    fclose(fp);
+
+    // recv();
+    // close();
+
+    exit(0);
+}
--- Day_32/basic/server.cpp
+++ Day_32/basic/server.cpp
@@ -4,11 +4,31 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <ctime>
+#include <cstring>
 #include "proto.h"
 
+const int ipstrsize = 40;
+const int bufsize = 1024;
+
+static void server_job(int tmp_sd){
+    char buf[bufsize];
+    size_t len;
+
+    sprintf(buf, fmt_stamp, (long long)time(nullptr));
+    len = strlen(buf);
+
+    if (send(tmp_sd, buf, len, 0) < 0){
+        perror("send");
+        exit(1);
+    }
+}
+
 int main(){
-    int sd;
-    struct sockaddr_in sockaddrIn{};
+    int sd, tmp_sd;
+    socklen_t raddr_len;
+    struct sockaddr_in laddr{}, raddr{};
+    char ipstr[ipstrsize];
 
     sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd < 0){
@@ -16,12 +36,12 @@ int main(){
         exit(1);
     }
 
-    sockaddrIn.sin_family = AF_INET;
-    sockaddrIn.sin_port = htons(server_port);
-    inet_pton(AF_INET, "0.0.0.0", &sockaddrIn.sin_addr);
+    laddr.sin_family = AF_INET;
+    laddr.sin_port = htons(server_port);
+    inet_pton(AF_INET, "0.0.0.0", &laddr.sin_addr);
 
 
-    if (bind(sd, (struct sockaddr *)&sockaddrIn, sizeof(sockaddrIn))<0){
+    if (bind(sd, (struct sockaddr *)&laddr, sizeof(laddr)) < 0){
         perror("bind");
         exit(1);
     }
@@ -31,10 +51,25 @@ int main(){
         perror("listen");
         exit(1);
     }
-    
-    accept();
 
-    send();
+    raddr_len = sizeof(raddr);
+    while(true) {
+        tmp_sd = accept(sd, (struct sockaddr *) &raddr, &raddr_len);
+        if ( tmp_sd < 0){
+            perror("accept");
+            exit(1);
+        }
+        inet_ntop(AF_INET, &raddr.sin_addr, ipstr, ipstrsize);
+        fprintf(stdout, "Client:%s:%d\n", ipstr, ntohs(raddr.sin_port));
 
-    close();
+        server_job(tmp_sd);
+
+        close(tmp_sd);
+    }
+
+    /*
+    close(sd);
+
+    exit(0);
+    */
 }
