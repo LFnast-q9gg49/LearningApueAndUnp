7e378b653e3102eb51cd460123660970b47b7a82
LFnast <luyuhao19@mails.ucas.ac.cn>
2023-04-10 20:00:00 +0800

    20230409

---
+++ Day_32/par/proto.h
@@ -0,0 +1,13 @@
+#ifndef PROTO_H__
+#define PROTO_H__
+
+const char *fmt_stamp   = "%lld\r\n";
+const int server_port = 2023;
+
+class proto {
+
+};
+
+
+#endif
+
---
+++ Day_32/par/server.cpp
@@ -0,0 +1,91 @@
+#include <cstdio>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctime>
+#include <cstring>
+#include "proto.h"
+
+const int ipstrsize = 40;
+const int bufsize = 1024;
+
+static void server_job(int tmp_sd){
+    char buf[bufsize];
+    size_t len;
+
+    sprintf(buf, fmt_stamp, (long long)time(nullptr));
+    len = strlen(buf);
+
+    if (send(tmp_sd, buf, len, 0) < 0){
+        perror("send");
+        exit(1);
+    }
+}
+
+int main(){
+    int sd, tmp_sd;
+    pid_t pid;
+    socklen_t raddr_len;
+    struct sockaddr_in laddr{}, raddr{};
+    char ipstr[ipstrsize];
+
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd < 0){
+        perror("socket\n");
+        exit(1);
+    }
+
+    int val = 1;
+    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0){
+        perror("setsockopt");
+        exit(1);
+    }
+
+    laddr.sin_family = AF_INET;
+    laddr.sin_port = htons(server_port);
+    inet_pton(AF_INET, "0.0.0.0", &laddr.sin_addr);
+
+
+    if (bind(sd, (struct sockaddr *)&laddr, sizeof(laddr)) < 0){
+        perror("bind");
+        exit(1);
+    }
+
+    // second value is the number of half-connect pool. Now is full connect num.
+    if (listen(sd, 200) < 0){
+        perror("listen");
+        exit(1);
+    }
+
+    raddr_len = sizeof(raddr);
+    while(true) {
+        tmp_sd = accept(sd, (struct sockaddr *) &raddr, &raddr_len);
+        if ( tmp_sd < 0){
+            perror("accept");
+            exit(1);
+        }
+
+        pid = fork();
+        if (pid < 0){
+            perror("fork");
+            exit(1);
+        }else if(pid == 0){
+	    close(sd);
+            inet_ntop(AF_INET, &raddr.sin_addr, ipstr, ipstrsize);
+            fprintf(stdout, "Client:%s:%d\n", ipstr, ntohs(raddr.sin_port));
+
+            server_job(tmp_sd);
+            close(tmp_sd);
+            exit(0);
+        }
+	    close(tmp_sd);        
+    }
+
+    /*
+    close(sd);
+
+    exit(0);
+    */
+}
