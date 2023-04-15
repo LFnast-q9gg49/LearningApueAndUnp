3cd476ced5b2a6d03e293ad48e360d66a4d0675e
LFnast <luyuhao19@mails.ucas.ac.cn>
2023-04-15 20:00:00 +0800

    20230415

--- Day_34/anytimer/anytimer.cpp
+++ Day_34/anytimer/anytimer.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cerrno>
+#include <signal.h>
+#include <sys/time.h>
 
 enum {
     STATE_RUNNING = 1,
@@ -17,6 +19,8 @@ struct at_job_st{
 };
 
 static struct at_job_st* job[job_max];
+static int init = 0;
+static struct sigaction old_sa;
 
 static int get_free_pos(){
     for (auto i = 0; i < job_max; i++){
@@ -25,6 +29,61 @@ static int get_free_pos(){
         }
     }
     return -1;
+}
+
+static void module_unload(){
+    struct itimerval timer{};
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+
+    setitimer(ITIMER_REAL, &timer, nullptr);
+
+    sigaction(SIGALRM, &old_sa, nullptr);
+}
+
+
+static void module_load(){
+    struct sigaction sa{};
+    struct itimerval timer{};
+
+    sa.sa_sigaction = [](int sig, siginfo_t *info, void *context) -> void{
+//        if (info->si_code != SI_KERNEL){
+//            return;
+//        }
+        for (auto & i : job){
+            if (i != nullptr){
+                if (i->job_state == STATE_RUNNING){
+                    i->time_remain--;
+                    if (i->time_remain <= 0){
+                        i->jobp(i->arg);
+                        i->job_state = STATE_OVER;
+                    }
+                }
+            }
+        }
+    };
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO; // three parameters
+
+    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_sec = 1;
+    timer.it_interval.tv_usec = 0;
+
+    if (sigaction(SIGALRM, &sa, &old_sa) < 0){
+        perror("sigaction()");
+        exit(1);
+    }
+
+
+    if (setitimer(ITIMER_REAL, &timer, nullptr) < 0){
+        perror("setitimer()");
+        exit(1);
+    }
+
+    atexit(module_unload);
 }
 
 int anytimer::at_addjob(int sec, void (*jobp)(void *), void *arg) {
@@ -36,6 +95,12 @@ int anytimer::at_addjob(int sec, void (*jobp)(void *), void *arg) {
     }
 
     auto me = new (struct at_job_st);
+
+    if (!init){
+        init = 1;
+        module_load();
+    }
+
     me->job_state = STATE_RUNNING;
     me->sec = sec;
     me->time_remain = me->sec;
@@ -52,13 +117,29 @@ int anytimer::at_canceljob(int id) {
         return -EINVAL;
     }
     if (job[id]->job_state == STATE_CANCELED ){
-
+        return -ECANCELED;
     }
+    if (job[id]->job_state == STATE_OVER){
+        return -EBUSY;
+    }
+    job[id]->job_state = STATE_CANCELED;
 
     return 0;
 }
 
 int anytimer::at_waitjob(int id) {
+    if (id < 0 || id > job_max || job[id] == nullptr){
+        return -EINVAL;
+    }
+    while (job[id]->job_state == STATE_RUNNING){
+        pause();
+    }
+
+    if (job[id]->job_state == STATE_CANCELED || job[id]->job_state == STATE_OVER){
+        delete job[id];
+        job[id] = nullptr;
+    }
+
     return 0;
 }
 
--- Day_34/anytimer/main.cpp
+++ Day_34/anytimer/main.cpp
@@ -1,14 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <csignal>
 #include "anytimer.h"
 
 void f1(void *ptr){
-
+    printf("f1():%s\n", ptr);
 }
 
 void f2(void *ptr){
-
+    printf("f2():%s\n", ptr);
 }
 
 int main() {
@@ -21,7 +22,13 @@ int main() {
     }
     job2 = anytimer::at_addjob(2, f2, (void *)"bbb");
     job3 = anytimer::at_addjob(7, f1, (void *)"ccc");
+
     puts("End!");
+
+    while (true){
+        write(1, ".", 1);
+        sleep(1);
+    }
 
 }
 
